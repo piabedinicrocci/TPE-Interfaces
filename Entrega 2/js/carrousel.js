33a270c48b6e2429b9af2b90566dbf0ebce7e7aa
“piabedinicrocci” <piabedinicrocci@hotmail.com>
2024-10-14 23:57:57 -0300

    finalizado index, home

--- Entrega 2/js/carrousel.js
+++ Entrega 2/js/carrousel.js
@@ -1,33 +1,32 @@
-const carousel = document.querySelector('.carrousel-juegos'); // Selecciona el contenedor de las tarjetas
-let isDragging = false; // Bandera para indicar si se está arrastrando
-let startX; // Posición inicial del mouse
-let scrollLeft; // Posición de desplazamiento inicial
+const carousels = document.querySelectorAll('.carrousel-juegos');
 
-// Evento mousedown
-carousel.addEventListener('mousedown', (e) => {
-    isDragging = true; // Cambia la bandera a true
-    startX = e.pageX - carousel.offsetLeft; // Captura la posición inicial del mouse
-    scrollLeft = carousel.scrollLeft; // Captura la posición actual del scroll
-    carousel.style.cursor = 'grabbing'; // Cambia el cursor a 'grabbing'
-});
+carousels.forEach(carousel => {
+    let isDragging = false;
+    let startX;
+    let scrollLeft;
 
-// Evento mouseleave
-carousel.addEventListener('mouseleave', () => {
-    isDragging = false; // Cambia la bandera a false al salir del área
-    carousel.style.cursor = 'grab'; // Cambia el cursor de vuelta
-});
+    carousel.addEventListener('mousedown', (e) => {
+        isDragging = true;
+        startX = e.pageX - carousel.offsetLeft;
+        scrollLeft = carousel.scrollLeft;
+        carousel.style.cursor = 'grabbing';
+    });
 
-// Evento mouseup
-carousel.addEventListener('mouseup', () => {
-    isDragging = false; // Cambia la bandera a false al soltar el botón del mouse
-    carousel.style.cursor = 'grab'; // Cambia el cursor de vuelta
-});
+    carousel.addEventListener('mouseleave', () => {
+        isDragging = false;
+        carousel.style.cursor = 'grab';
+    });
 
-// Evento mousemove
-carousel.addEventListener('mousemove', (e) => {
-    if (!isDragging) return; // Si no se está arrastrando, sale de la función
-    e.preventDefault(); // Previene la selección de texto
-    const x = e.pageX - carousel.offsetLeft; // Posición actual del mouse
-    const walk = (x - startX) * 1; // Determina cuánto se ha movido el mouse
-    carousel.scrollLeft = scrollLeft - walk; // Actualiza la posición del scroll
+    carousel.addEventListener('mouseup', () => {
+        isDragging = false;
+        carousel.style.cursor = 'grab';
+    });
+
+    carousel.addEventListener('mousemove', (e) => {
+        if (!isDragging) return;
+        e.preventDefault();
+        const x = e.pageX - carousel.offsetLeft;
+        const walk = (x - startX) * 1;
+        carousel.scrollLeft = scrollLeft - walk;
+    });
 });
